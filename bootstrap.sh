0d92174dcbb1f5415f74c1490f3eda9fa6fb9cc3
Gavin Thomas Claugus <gclaugus@gmail.com>
2016-03-31 18:24:40 -0400

    Remove guest-utils package from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Clean up install script
+sudo rm /chroot.sh
+
 # Add archlinuxfr repo for yaourt access
 echo 'echo -e "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf' | sudo -s
 
@@ -7,7 +10,7 @@ echo 'echo -e "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.f
 sudo pacman -Syy
 
 # Install non-community packages
-sudo pacman -S --noconfirm --needed git neovim gvim rust cargo sqlite yaourt rsync virtualbox-guest-utils
+sudo pacman -S --noconfirm --needed git neovim gvim rust cargo sqlite yaourt rsync
 
 # Install community packages
 yaourt -S --noconfirm vcsh
