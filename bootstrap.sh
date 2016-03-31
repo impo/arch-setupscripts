29265733eb1f32cf5f79058a23979445ca099dfc
Gavin Thomas Claugus <gclaugus@gmail.com>
2016-03-31 11:14:31 -0400

    Fix archlinuxfr repo again.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Add archlinuxfr repo for yaourt access
-sudo -s 'echo -e "[archlinuxfr]\nServer = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf'
+echo 'echo -e "[archlinuxfr]\nServer = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf' | sudo -s
+
+# Refresh pacman repos
+sudo pacman -Syy
 
 # Install non-community packages
 sudo pacman -S --noconfirm --needed git neovim gvim rust cargo sqlite yaourt rsync
