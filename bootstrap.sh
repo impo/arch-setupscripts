5de3b4e7bc5eae1b81d88423a80161997900021c
Gavin Thomas Claugus <gclaugus@gmail.com>
2016-03-31 16:12:02 -0400

    Install virtualbox-guest-utils package

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ echo 'echo -e "[archlinuxfr]\nSigLevel = Never\nServer = http://repo.archlinux.f
 sudo pacman -Syy
 
 # Install non-community packages
-sudo pacman -S --noconfirm --needed git neovim gvim rust cargo sqlite yaourt rsync
+sudo pacman -S --noconfirm --needed git neovim gvim rust cargo sqlite yaourt rsync virtualbox-guest-utils
 
 # Install community packages
 yaourt -S --noconfirm vcsh
